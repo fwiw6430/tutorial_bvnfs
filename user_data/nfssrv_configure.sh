24086dab1736ef3fca16a4f7fb5495a2c88999fc
Tsutomu Miyashita <tsutomu.miyashita@oracle.com>
2025-05-20 15:39:55 +0900

    Modify PF image to the latest 810 20250416

--- user_data/nfssrv_configure.sh
+++ user_data/nfssrv_configure.sh
@@ -17,22 +17,22 @@ for ((i = 0; i < ${#ar_bv_dev_name[@]}; i++))
   done
 }
 
+echo "Start configuring secondary NIC"
+sudo systemctl enable --now ocid.service
 echo "Start creating volume group"
 sudo vgcreate bv $bv_dev_list
 echo "Start creating logical volume"
 sudo lvcreate -y -l 100%FREE --stripes 15 --stripesize "64K" -n bv bv
 echo "Start formatting XFS file system"
 sudo mkfs.xfs -L blockvolume /dev/bv/bv
+echo "Start mounting file system"
 echo "LABEL=blockvolume /mnt/bv/ xfs defaults,_netdev,noatime 0 0" | sudo tee -a /etc/fstab
 sudo systemctl daemon-reload
 sudo mkdir -p /mnt/bv
-echo "Start mounting file system"
 sudo mount /mnt/bv
-echo "Start configuring secondary NIC"
-sudo systemctl enable --now ocid.service
+echo "Start NFS server"
 echo "/mnt/bv (rw,sync,no_root_squash)" | sudo tee -a /etc/exports
 sudo sed -i 's/# threads=8/threads=64/g' /etc/nfs.conf
-echo "Start NFS server"
 sudo systemctl enable --now nfs-server rpcbind
 echo "Start changing kernel to RHCK"
 vmlinuz=`sudo grubby --info=ALL | grep ^kernel= | grep -v -e uek -e rescue | awk -F\" '{print $2}'`
